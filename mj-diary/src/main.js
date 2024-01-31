05cd998a5172bd47a25e1230b06dd367b53b2ccd
min-minnn <alswlk0306@gmail.com>
2024-01-31 23:41:32 +0900

    add: install axios

--- mj-diary/src/main.js
+++ mj-diary/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store.js'
 import router from './router.js'
+import axios from 'axios'
 
 const app = createApp(App)
+
+app.config.globalProperties.axios = axios;
 
 app.use(router).use(store).mount('#app')
