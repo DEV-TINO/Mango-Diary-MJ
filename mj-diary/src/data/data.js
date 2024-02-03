3afeaff338c48fb76841f2c35c697fe3a9acece3
min-minnn <alswlk0306@gmail.com>
2024-02-03 16:12:49 +0900

    add: emoji, post, and statistics data

---
+++ mj-diary/src/data/data.js
@@ -0,0 +1,84 @@
+const data = {
+	emoji: [
+		{
+      emoji_id: 0,
+      emoji_color_type: "gray",
+      emoji_name: "happiness",
+      emoji_subtitle: "행복해!",
+      emoji_image: "/mood/happiness.png"
+    },
+		{
+      emoji_id: 1,
+      emoji_color_type: "gray",
+      emoji_name: "angry",
+      emoji_subtitle: "화나!",
+      emoji_image: "/mood/angry.png"
+    },
+		{
+      emoji_id: 2,
+      emoji_color_type: "gray",
+      emoji_name: "depressed",
+      emoji_subtitle: "우울해..",
+      emoji_image: "/mood/depressed.png"
+    },
+		{
+      emoji_id: 3,
+      emoji_color_type: "gray",
+      emoji_name: "happy",
+      emoji_subtitle: "너무 기뻐!!",
+      emoji_image: "/mood/happy.png"
+    },
+		{
+      emoji_id: 4,
+      emoji_color_type: "gray",
+      emoji_name: "sad",
+      emoji_subtitle: "슬퍼..",
+      emoji_image: "/mood/sad.png"
+    }
+	],
+	post: [
+		{
+			post_id: 0,
+			post_year: "2019",
+			post_month: "01",
+			post_date: "03",
+			post_emoji_id: 0,
+			post_content: "내용",
+			post_upload_image: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"
+		}
+	],
+	statistics: [
+		{
+			statistic_rank: 1,
+			statistic_emoji_id: 0,
+			statistic_count: 15,
+			statistic_comment: "마음속에 따스한 햇살이 내리쬐는 것처럼 편안하고 만족스러운 달이었나 봐요."
+		},
+		{
+			statistic_rank: 2,
+			statistic_emoji_id: 2,
+			statistic_count: 10,
+			statistic_comment: "희망 없이 침울하고 답답한 감정에 휩싸이는 날이 많았나 봐요."
+		},
+		{
+			statistic_rank: 3,
+			statistic_emoji_id: 1,
+			statistic_count: 3,
+			statistic_comment: "불쾌하거나 억울한 감정으로 인해 마음이 격앙되는 달이었나 봐요."
+		},
+		{
+			statistic_rank: 4,
+			statistic_emoji_id: 3,
+			statistic_count: 2,
+			statistic_comment: "즐겁고 흐뭇한 감정으로 인해 마음이 들뜨는 날이 많았나 봐요."
+		},
+		{
+			statistic_rank: 5,
+			statistic_emoji_id: 4,
+			statistic_count: 1,
+			statistic_comment: "슬프고 괴로운 감정으로 인해 마음이 울적해지는 달이었나 봐요."
+		}
+	]
+}
+
+export default data;
