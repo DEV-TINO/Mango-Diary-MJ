b3e851b7b5f544689a2b20ef876b74b9b1201925
min-minnn <alswlk0306@gmail.com>
2024-02-18 01:45:30 +0900

    style: data format

--- mj-diary/src/data/data.js
+++ mj-diary/src/data/data.js
@@ -1,82 +1,67 @@
 const data = {
 	emoji: [
 		{
-      emoji_id: 0,
-      emoji_color_type: "gray",
-      emoji_name: "happiness",
-      emoji_subtitle: "행복",
-      emoji_image: "/mood/happiness.png"
+      id: 0,
+      name: "pleased",
+      subtitle: "기쁨"
     },
 		{
-      emoji_id: 1,
-      emoji_color_type: "gray",
-      emoji_name: "angry",
-      emoji_subtitle: "화남",
-      emoji_image: "/mood/angry.png"
+      id: 1,
+      name: "angry",
+      subtitle: "화남"
     },
 		{
-      emoji_id: 2,
-      emoji_color_type: "gray",
-      emoji_name: "depressed",
-      emoji_subtitle: "우울",
-      emoji_image: "/mood/depressed.png"
+      id: 2,
+      name: "depressed",
+      subtitle: "우울"
     },
 		{
-      emoji_id: 3,
-      emoji_color_type: "gray",
-      emoji_name: "happy",
-      emoji_subtitle: "기쁨",
-      emoji_image: "/mood/happy.png"
+      id: 3,
+      name: "happy",
+      subtitle: "행복"
     },
 		{
-      emoji_id: 4,
-      emoji_color_type: "gray",
-      emoji_name: "sad",
-      emoji_subtitle: "슬픔",
-      emoji_image: "/mood/sad.png"
+      id: 4,
+      name: "sad",
+      subtitle: "슬픔"
     }
 	],
 	post: [
 		{
-			post_id: "20240201",
-			post_year: "2024",
-			post_month: "02",
-			post_date: "01",
-			post_emoji_id: 3,
-			post_content: "안녕 친구들 ~ 안녕 친구들 ~",
-			post_upload_image: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"
+			id: "20240201",
+			year: "2024",
+			month: "02",
+			date: "01",
+			id: 3,
+			content: "안녕 친구들 ~ 안녕 친구들 ~",
+			image: "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png"
 		}
 	],
-	statistics: [
+	statistic: [
 		{
-			statistic_rank: 1,
-			statistic_emoji_id: 0,
-			statistic_count: 15,
-			statistic_comment: "마음속에 따스한 햇살이 내리쬐는 것처럼 편안하고 만족스러운 달이었나 봐요."
+			emoji: 'pleased',
+			count: 1,
+			comment: "마음속에 따스한 햇살이 내리쬐는 것처럼 편안하고 만족스러운 달이었나 봐요."
 		},
 		{
-			statistic_rank: 2,
-			statistic_emoji_id: 2,
-			statistic_count: 10,
-			statistic_comment: "희망 없이 침울하고 답답한 감정에 휩싸이는 날이 많았나 봐요."
+			emoji: 'depressed',
+			count: 5,
+			comment: "희망 없이 침울하고 답답한 감정에 휩싸이는 날이 많았나 봐요."
 		},
 		{
-			statistic_rank: 3,
-			statistic_emoji_id: 1,
-			statistic_count: 3,
-			statistic_comment: "불쾌하거나 억울한 감정으로 인해 마음이 격앙되는 달이었나 봐요."
+			emoji: 'angry',
+			count: 2,
+			comment: "불쾌하거나 억울한 감정으로 인해 마음이 격앙되는 달이었나 봐요."
 		},
 		{
-			statistic_rank: 4,
-			statistic_emoji_id: 3,
-			statistic_count: 2,
-			statistic_comment: "즐겁고 흐뭇한 감정으로 인해 마음이 들뜨는 날이 많았나 봐요."
+			emoji: 'happy',
+			count: 6,
+			comment: "즐겁고 흐뭇한 감정으로 인해 마음이 들뜨는 날이 많았나 봐요."
 		},
 		{
-			statistic_rank: 5,
-			statistic_emoji_id: 4,
-			statistic_count: 1,
-			statistic_comment: "슬프고 괴로운 감정으로 인해 마음이 울적해지는 달이었나 봐요."
+			emoji: 'sad',
+			count: 9,
+			comment: "슬프고 괴로운 감정으로 인해 마음이 울적해지는 달이었나 봐요."
 		}
 	]
 }
