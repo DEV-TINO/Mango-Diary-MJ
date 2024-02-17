fd049a35c9630f129a44e7457e74d300a4897a88
min-minnn <alswlk0306@gmail.com>
2024-02-18 07:54:54 +0900

    style: change statistics count zero

--- mj-diary/src/data/data.js
+++ mj-diary/src/data/data.js
@@ -40,27 +40,27 @@ const data = {
 	statistic: [
 		{
 			emoji: 'pleased',
-			count: 1,
+			count: 0,
 			comment: "마음속에 따스한 햇살이 내리쬐는 것처럼 편안하고 만족스러운 달이었나 봐요."
 		},
 		{
 			emoji: 'depressed',
-			count: 5,
+			count: 0,
 			comment: "희망 없이 침울하고 답답한 감정에 휩싸이는 날이 많았나 봐요."
 		},
 		{
 			emoji: 'angry',
-			count: 2,
+			count: 0,
 			comment: "불쾌하거나 억울한 감정으로 인해 마음이 격앙되는 달이었나 봐요."
 		},
 		{
 			emoji: 'happy',
-			count: 6,
+			count: 0,
 			comment: "즐겁고 흐뭇한 감정으로 인해 마음이 들뜨는 날이 많았나 봐요."
 		},
 		{
 			emoji: 'sad',
-			count: 9,
+			count: 0,
 			comment: "슬프고 괴로운 감정으로 인해 마음이 울적해지는 달이었나 봐요."
 		}
 	]
